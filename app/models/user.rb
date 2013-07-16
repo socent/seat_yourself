015937ae6d929cac228523a598fa28644b9a719b
Jin <hojin.yoo@outlook.com>
2013-07-16 16:29:16 -0400

    adding associations to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password_digest
+  attr_accessible :email, :password, :password_confirmation, :name
+  has_secure_password
+  validates_presence_of :password, :on => :create
+
+  has_many :reservations
+  has_many :restaurants, foreign_key: :owner_id
 end
