ae44c74d1b8ed8062cfd02c5792f03dd6b7e21ca
Jin <hojin.yoo@outlook.com>
2013-07-16 16:54:36 -0400

    adding few actions for restaurants and views for restaurants/users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,18 @@
 class RestaurantsController < ApplicationController
+
+  def new
+    # pull from data scraping
+  end
+
+  def create
+  end
+
+  def index
+    @restaurants = Restaurants.all
+  end
+
+  def show
+    @restaurant = Restaurants.find(params[:restaurant])
+  end
+
 end
